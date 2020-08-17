60dee536fed4688496ab5c9d2a8cb92a975cca59
Lucas Vieira Vicente <lucasvieiravicente1@gmail.com>
2020-08-17 19:00:11 -0300

    Removed not used controller/class and create game controller

---
+++ Data/Repository/BaseRepository.cs
@@ -0,0 +1,36 @@
+﻿using ApiLucasVieiraVicente.Data.Context;
+using ApiLucasVieiraVicente.Data.Repository.Interfaces;
+using ApiLucasVieiraVicente.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiLucasVieiraVicente.Data.Repository
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        internal GameContext Context;
+        internal DbSet<T> DbSet;
+
+        public BaseRepository(GameContext context)
+        {
+            Context = context;
+            DbSet = Context.Set<T>();
+        }
+
+        public T GetById(Guid id) => DbSet.Find(id);
+
+        public IEnumerable<T> GetAll() => DbSet;
+
+        public void Add(T entity) => DbSet.Add(entity);
+
+        public void Update(T entity) => DbSet.Update(entity);
+
+        public void Remove(T entity) => DbSet.Remove(entity);
+
+        public void Remove(Guid id) => Remove(GetById(id));        
+    }
+}
---
+++ Data/Repository/GameRepository.cs
@@ -0,0 +1,21 @@
+﻿using ApiLucasVieiraVicente.Data.Context;
+using ApiLucasVieiraVicente.Data.Repository.Interfaces;
+using ApiLucasVieiraVicente.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiLucasVieiraVicente.Data.Repository
+{
+    public class GameRepository : BaseRepository<Game>
+    {
+        public GameRepository(GameContext context) : base(context)
+        {
+        }
+
+        public Game GetByName(string name) => base.DbSet.Where(w => w.Name.ToUpper() == name.ToUpper()).FirstOrDefault();
+
+        public IEnumerable<Game> GetByGenre(GameGenre genre) => base.DbSet.Where(w => w.GameGenre == genre);
+    }
+}
